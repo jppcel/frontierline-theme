c0ab2bda243ca7505125053677be4c355f1779f1
João Paulo Polles <joaopaulopolles@gmail.com>
2017-07-30 23:29:30 -0300

    Created a nav from wp

--- themes/frontierline-brasil/includes/nav-global.php
+++ themes/frontierline-brasil/includes/nav-global.php
@@ -11,10 +11,14 @@
     <div class="nav-mozilla">
       <span class="toggle" role="button" aria-controls="nav-mozilla-menu" aria-expanded="false" tabindex="0"><?php _e('Menu', 'frontierline'); ?></span>
       <ul class="nav-mozilla-menu" id="nav-mozilla-menu">
-        <li class="nav-global-health"><a href="https://www.mozilla.org/internet-health/" rel="external"><?php _e('Internet Health', 'frontierline'); ?></a></li>
-        <li class="nav-global-tech"><a href="https://www.mozilla.org/technology/" rel="external"><?php _e('Web Innovations', 'frontierline'); ?></a></li>
-        <li class="nav-global-donate"><a href="https://donate.mozilla.org/" rel="external"><?php _e('Give', 'frontierline'); ?></a></li>
-        <li class="nav-global-firefox"><a href="https://www.mozilla.org/firefox/products/" rel="external"><?php _e('Discover Firefox', 'frontierline'); ?></a></li>
+        <?php
+        wp_nav_menu(array(
+             'container' => false,                           // Remove nav container
+             'theme_location' => 'main-nav',        			// Where it's located in the theme
+             'depth' => 5,                                   // Limit the depth of the nav
+             'fallback_cb' => false                         // Fallback function (see below)
+         ));
+        ?>
       </ul>
     </div>
 
